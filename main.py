219183a28f0259ad04875e3a637ab5824c0e600b
YanaYanovna <manenok.88@mail.ru>
2025-06-14 22:23:06 +0500

    Changed to full prompt

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ seeds = [42, 100, 2024, 999, 777]
 
 loaded_models = []
 for seed in seeds:
-    model_path = os.path.join(save_dir, f"catboost_model_log_{seed}_14_2405.cbm")
+    model_path = os.path.join(save_dir, f"catboost_model_log_{seed}_143.cbm")
     model = CatBoostRegressor()
     model.load_model(model_path)
     loaded_models.append(model)
@@ -145,7 +145,7 @@ def predict(X_test, username):
         "priceInUSD": int(pred_mean),
         "confidence": int(round(confidence_score)),
         "score": int(round(score)),
-        "version": "1.4.2"
+        "version": "1.4.3"
     }
 
     return result
